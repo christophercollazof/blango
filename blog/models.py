e8d4c148c6824bab96064379a6ddc1165235eb3b
Christopher Collazo <christopher.collazof@outlook.com>
2024-03-15 22:39:14 -0600

    Finish html framewokrs

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.conf import settings
-from django.utils import timezone
 from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelation
 from django.contrib.contenttypes.models import ContentType
 
