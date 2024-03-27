7a94ce7e9abb778f6d2fd75510849642820c2191
Christopher Collazo <christopher.collazof@outlook.com>
2024-03-27 12:27:42 -0600

    Finish Django Configuration

--- blango/urls.py
+++ blango/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
 import blog.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", blog.views.index),
-    path("post/<slug>/", blog.views.post_detail, name="blog-post-detail")
+    path("post/<slug>/", blog.views.post_detail, name="blog-post-detail"),
 ]
-
--- blog/forms.py
+++ blog/forms.py
@@ -4,11 +4,12 @@ from crispy_forms.helper import FormHelper
 from blog.models import Comment
 
 class CommentForm(forms.ModelForm):
-  class Meta:
-    model = Comment
-    fields = ["content"]
+    class Meta:
+        model = Comment
+        fields = ["content"]
 
-  def __init__(self, *args, **kwargs):
-    super().__init__(*args, **kwargs)
-    self.helper = FormHelper()
-    self.helper.add_input(Submit('submit', 'Submit'))
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.add_input(Submit('submit', 'Submit'))
+        
